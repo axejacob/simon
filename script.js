3ab32b6e633a7038519efe4c258b0177c6f34f03
Axel <email@axeljacob.com>
2019-07-08 08:58:03 +0800

    Done with CSS version 1(very primitive looking). HTML done(rendered a new sound-clip and hosted it on Digitalocean. JS quite done: -click is responsive with sound -clickable color
    Sound-clip is actually quite long. need to rendered it shorter.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Declared all variable
 let order = [];
 let playerOrder = [];
 let on = false;
@@ -63,3 +64,152 @@ function play(){
         order.push(Math.floor(Math.random() * 4) + 1);
     }
     compTurn = true;
+
+//Computer flash light every 1 sec. setInterval will run gameTurn every 1 sec. It will keep repeating until intervalId is cleared.
+    intervalId = setInterval(gameTurn, 1000);
+};
+
+//define the gameTurn function: Player can't click buttons when the computer is flashing lights.
+function gameTurn(){
+    on = false;
+
+    if(flash == turn){
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+
+    if (compTurn){
+        clearColor();
+        setTimeout(() => {
+            if(order[flash] == 1) one();
+            if(order[flash] == 2) two();
+            if(order[flash] == 3) three();
+            if(order[flash] == 4) four();
+            flash++;
+        }, 200);
+    }
+}
+
+function one(){
+    if (sound){
+        let audio = document.getElementById('sound1');
+        audio.play();
+    }
+    sound = true;
+    topL.style.backgroundColor = 'lightblue';
+}
+
+function two(){
+    if (sound){
+        let audio = document.getElementById('sound2');
+        audio.play();
+    }
+    sound = true;
+    topR.style.backgroundColor = 'darkred';
+}
+
+function three(){
+    if (sound){
+        let audio = document.getElementById('sound3');
+        audio.play();
+    }
+    sound = true;
+    bottomL.style.backgroundColor = 'yellow';
+}
+
+function four(){
+    if (sound){
+        let audio = document.getElementById('sound4');
+        audio.play();
+    }
+    sound = true;
+    bottomR.style.backgroundColor = 'lightgreen';
+}
+
+function clearColor(){
+    topL.style.backgroundColor = '#0057e7';
+    topR.style.backgroundColor = '#d62d20';
+    bottomL.style.backgroundColor = '#008744';
+    bottomR.style.backgroundColor = '#ffa700';
+}
+
+topL.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(1);
+        // check();
+        one();
+        if(!win){
+            setTimeout(() => {
+                clearColor();
+            }, 300)
+        }
+    }
+})
+
+topR.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(2);
+        // check();
+        two();
+        if(!win){
+            setTimeout(() => {
+                clearColor();
+            }, 300)
+        }
+    }
+})
+
+bottomL.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(3);
+        // check();
+        three();
+        if(!win){
+            setTimeout(() => {
+                clearColor();
+            }, 300)
+        }
+    }
+})
+
+bottomR.addEventListener('click', (event) => {
+    if (on) {
+        playerOrder.push(4);
+        // check();
+        four();
+        if(!win){
+            setTimeout(() => {
+                clearColor();
+            }, 300)
+        }
+    }
+})
+
+function check(){
+    if(playerOrder[playerOrder.length - 1] !==[playerOrder.length - 1]) good = false;
+    if(playerOrder.length == 20 && good == true) {
+        winGame();
+    }
+    if(good == false){
+        flashColor();
+        turnCounter.innerHTML = 'NO!';
+        setTimeout(() => {
+            turnCounter.innerHTML = turn;
+            clearColor();
+
+        if(fierce){
+            play();
+        }  else {
+            compTurn = true;
+            flash = 0;
+            playerOrder = [];
+            good = true;
+            intervalId = setInterval(gameTurn, 1000);
+        }
+        }, 700);
+
+        sound = false;
+    }
+}
